3712dd0b14ed5457b1057c00cd2ffadeffc92103
Ludchenko <skillet367@icloud.com>
2020-05-03 21:19:19 +0300

    fixed mvn package

--- src/main/java/mate/academy/internetshop/service/interfaces/GenericService.java
+++ src/main/java/mate/academy/internetshop/service/interfaces/GenericService.java
@@ -1,7 +1,6 @@
 package mate.academy.internetshop.service.interfaces;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface GenericService<T, K> {
     T create(T element);
--- src/main/java/mate/academy/internetshop/service/interfaces/ProductService.java
+++ src/main/java/mate/academy/internetshop/service/interfaces/ProductService.java
@@ -1,6 +1,5 @@
 package mate.academy.internetshop.service.interfaces;
 
-import java.util.List;
 import mate.academy.internetshop.model.Product;
 
 public interface ProductService extends GenericService<Product, Long> {
